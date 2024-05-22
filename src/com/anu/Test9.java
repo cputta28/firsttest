b8ab222902564bcde4aeddf65e8c01d65efd7c1f
cputta28 <chandrashekar.putta23@gmail.com>
2024-05-22 19:53:52 +0530

    test app

---
+++ src/com/anu/Test9.java
@@ -0,0 +1,12 @@
+package com.anu;
+
+import java.time.LocalDate;
+
+public class Test9 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello welcome to github"+LocalDate.now());
+
+	}
+
+}
